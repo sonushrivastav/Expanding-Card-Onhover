f487c25d9756be0c14abe62ba4546935545f5a7e
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-12 12:22:37 +0530

    adding the html part

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,20 @@ function App() {
 
   return (
     <div className='container'>
-      <div>
-        <h3></h3>
+      <div className='panel active' style="background-image: url('./assets/Anime-1.jpg')">
+        <h3>I don't remember the name</h3>
+      </div>
+      <div className='panel' style="background-image: url('./assets/Anime-2.jpg')">
+        <h3>Sword art online</h3>
+      </div>
+      <div className='panel' style="background-image: url('./assets/Anime-3.jpg')">
+        <h3>Forgot the name tbh</h3>
+      </div>
+      <div className='panel' style="background-image: url('./assets/Anime-4.jpg')">
+        <h3>Demon Slayer : Tanjiro</h3>
+      </div>
+      <div className='panel' style="background-image: url('./assets/Anime-5.jpg')">
+        <h3>One and Only : Naruto</h3>
       </div>
     </div>
   )
