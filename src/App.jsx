5b959f2e2ed1518881f029774102dbee7700026c
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-12 15:36:26 +0530

    adding logic for card expantion

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,50 @@
-import React,{ useEffect } from 'react'
-import { useRef } from 'react'
-import './App.css'
-import img1 from "./assets/Anime-1.jpg"
-import img2 from "./assets/Anime-2.jpg"
-import img3 from "./assets/Anime-3.jpg"
-import img4 from "./assets/Anime-4.jpg"
-import img5 from "./assets/Anime-5.jpg"
+import React, { useEffect } from "react";
+import { useRef } from "react";
+import "./App.css";
+import img1 from "./assets/Anime-1.jpg";
+import img2 from "./assets/Anime-2.jpg";
+import img3 from "./assets/Anime-3.jpg";
+import img4 from "./assets/Anime-4.jpg";
+import img5 from "./assets/Anime-5.jpg";
 
 function App() {
-  const hoverRef = useRef()
-  let panels = hoverRef.current
-  //  console.log(hoverRef.current.classList)
-    panels?.forEach((panel) => {
-      panel.addEventListener("click", () => {
-        removeActiveclasses();
-        panel.classList.add("active")
-     })
-   })
+  const elements = document.querySelectorAll(".panel");
 
-  const removeActiveclasses = () => {
-    panels?.forEach((panel) => {
-      panel.classList.remove("active")
-    })
-  }
-  // useEffect(() => {
-  //   onhoverOnCard()
-  // },[])
+  useEffect(() => {
+    elements.forEach((panel) => {
+      console.log(panel);
+      panel.addEventListener("click", () => {
+        removeActiveclass();
+        panel?.classList.add("active");
+      });
+    });
+  }, []);
+
+  const removeActiveclass = () => {
+    elements.forEach((panel) => {
+      panel?.classList.remove("active");
+    });
+  };
 
   return (
-    <div className='container'>
-      <div className='panel active' ref={hoverRef} style={{backgroundImage: `url(${img1})`}}>
+    <div className="container">
+      <div className="panel active" style={{ backgroundImage: `url(${img1})` }}>
         <h3>I don't remember the name</h3>
       </div>
-      <div className='panel' ref={hoverRef} style={{backgroundImage: `url(${img2})`}}>
+      <div className="panel" style={{ backgroundImage: `url(${img2})` }}>
         <h3>Sword art online</h3>
       </div>
-      <div className='panel' ref={hoverRef} style={{backgroundImage: `url(${img3})`}}>
+      <div className="panel" style={{ backgroundImage: `url(${img3})` }}>
         <h3>Forgot the name tbh</h3>
       </div>
-      <div className='panel' ref={hoverRef} style={{backgroundImage: `url(${img4})`}}>
+      <div className="panel" style={{ backgroundImage: `url(${img4})` }}>
         <h3>Demon Slayer : Tanjiro</h3>
       </div>
-      <div className='panel' ref={hoverRef} style={{backgroundImage: `url(${img5})`}}>
-        <h3>One and Only : Naruto</h3>
+      <div className="panel" style={{ backgroundImage: `url(${img5})` }}>
+        <h3>One and Only : Goku</h3>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
