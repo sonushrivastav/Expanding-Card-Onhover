8c66fe4809cbccad19260f71e76cf55829efbeec
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-12 12:51:40 +0530

    adding inline css to react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { useState } from 'react'
+import React,{ useState } from 'react'
 import './App.css'
+import img1 from "./assets/Anime-1.jpg"
+import img2 from "./assets/Anime-2.jpg"
+import img3 from "./assets/Anime-3.jpg"
+import img4 from "./assets/Anime-4.jpg"
+import img5 from "./assets/Anime-5.jpg"
 
 function App() {
   
 
   return (
     <div className='container'>
-      <div className='panel active' style="background-image: url('./assets/Anime-1.jpg')">
+      <div className='panel active' style={{backgroundImage: `url(${img1})`}}>
         <h3>I don't remember the name</h3>
       </div>
-      <div className='panel' style="background-image: url('./assets/Anime-2.jpg')">
+      <div className='panel' style={{backgroundImage: `url(${img2})`}}>
         <h3>Sword art online</h3>
       </div>
-      <div className='panel' style="background-image: url('./assets/Anime-3.jpg')">
+      <div className='panel' style={{backgroundImage: `url(${img3})`}}>
         <h3>Forgot the name tbh</h3>
       </div>
-      <div className='panel' style="background-image: url('./assets/Anime-4.jpg')">
+      <div className='panel' style={{backgroundImage: `url(${img4})`}}>
         <h3>Demon Slayer : Tanjiro</h3>
       </div>
-      <div className='panel' style="background-image: url('./assets/Anime-5.jpg')">
+      <div className='panel' style={{backgroundImage: `url(${img5})`}}>
         <h3>One and Only : Naruto</h3>
       </div>
     </div>
